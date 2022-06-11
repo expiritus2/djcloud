b697762c96595bb8857f46b690c004b91c1640fc
Mikhail Drozdovich <mikhail.drozdovich@bluestone.com.au>
2022-06-12 00:19:38 +0300

    Try head

--- frontend/src/api/user/index.ts
+++ frontend/src/api/user/index.ts
@@ -1,5 +1,5 @@
-import { apiServer } from '../../settings/web-services/api';
-import { LoginProps } from '../../store/User/types';
+import { apiServer } from 'settings/web-services/api';
+import { LoginProps } from 'store/User/types';
 
 export const login = (cfg: LoginProps) => {
     return apiServer.post('/auth/signin', cfg);

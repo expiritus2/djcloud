097bda5cb511012eecfa4f911e326bc0e655c41a
Mikhail Drozdovich <mikhail.drozdovich@bluestone.com.au>
2022-07-17 01:34:39 +0300

    Add dotenv to migrations

--- backend/src/lib/configs/app/cookieSession.ts
+++ backend/src/lib/configs/app/cookieSession.ts
@@ -8,7 +8,7 @@ export const setCookieSession = (app: INestApplication) => {
             name: process.env.COOKIE_SESSION_NAME || 'session',
             keys: [process.env.COOKIE_KEY],
             expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30), // one month
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : false,
+            sameSite: false,
         }),
     );
 };

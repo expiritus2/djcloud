6f9898f54332608e16e7e59cedcb762567b18920
Mikhail Drozdovich <mikhail.drozdovich@bluestone.com.au>
2022-07-17 00:58:15 +0300

    Add dotenv to migrations

--- backend/migrations/development/1652470034929-set-admin.js
+++ backend/migrations/development/1652470034929-set-admin.js
@@ -4,7 +4,7 @@ module.exports = class setAdmin1652470034929 {
     async up(queryRunner) {
         queryRunner.query(`
         insert into users (email, password, "roleId")
-        values ('test@email.com', '486e9b3962de2e17c1c399596d1ffd508bf163cdaf7ec38cb22694e44c016093',
+        values ('test@email.com', '6bc5655cd635355edbdc640c3809210770b89dcc36d66a1a687ee2b8c39208e6',
                 (select id from roles where name = 'admin'))
     `);
     }
@@ -14,7 +14,7 @@ module.exports = class setAdmin1652470034929 {
         delete
         from users
         where email = 'test@email.com'
-          and password = '486e9b3962de2e17c1c399596d1ffd508bf163cdaf7ec38cb22694e44c016093'
+          and password = '6bc5655cd635355edbdc640c3809210770b89dcc36d66a1a687ee2b8c39208e6'
     `);
     }
 };
--- backend/migrations/production/1658005083954-set-admin.js
+++ backend/migrations/production/1658005083954-set-admin.js
@@ -1,12 +1,10 @@
-console.log('thisthisthis', process.env.ADMIN_EMAIL, process.env.ADMIN_PASSWORD);
-
 module.exports = class setAdmin1658005083954 {
     name = 'setAdmin1658005083954';
 
     async up(queryRunner) {
         queryRunner.query(`
         insert into users (email, password, "roleId")
-        values ('fryyystandoff@gmail.com', '7b421e7c97f56e2fe0418bfe6dbd7662cb0ab2b91495e9651fb616953cbbebbd',
+        values ('${process.env.ADMIN_EMAIL}', '${process.env.ADMIN_PASSWORD}',
                 (select id from roles where name = 'admin'))
     `);
     }
@@ -15,8 +13,8 @@ module.exports = class setAdmin1658005083954 {
         queryRunner.query(`
         delete
         from users
-        where email = ${process.env.ADMIN_EMAIL}
-          and password = ${process.env.ADMIN_PASSWORD}
+        where email = '${process.env.ADMIN_EMAIL}'
+          and password = '${process.env.ADMIN_PASSWORD}'
     `);
     }
 };

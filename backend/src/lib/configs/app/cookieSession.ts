f918f459f3d2b1b4d913b9c06c651b8245219192
Mikhail Drozdovich <mikhail.drozdovich@bluestone.com.au>
2022-07-17 02:19:24 +0300

    Add dotenv to migrations

--- backend/src/lib/configs/app/cookieSession.ts
+++ backend/src/lib/configs/app/cookieSession.ts
@@ -2,7 +2,6 @@ import { INestApplication } from '@nestjs/common';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const cookieSession = require('cookie-session');
 
-console.log('thisthisthis', process.env.COOKIE_SESSION_NAME, process.env.COOKIE_KEY);
 export const setCookieSession = (app: INestApplication) => {
     app.use(
         cookieSession({
@@ -11,7 +10,7 @@ export const setCookieSession = (app: INestApplication) => {
             expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30), // one month
             secure: true,
             httpOnly: true,
-            sameSite: false,
+            sameSite: 'none',
         }),
     );
 };

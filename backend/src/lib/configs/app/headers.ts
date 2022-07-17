e99acfaaab9d34d04f4e79362834fb9c5e7d873e
Mikhail Drozdovich <mikhail.drozdovich@bluestone.com.au>
2022-07-17 12:40:26 +0300

    Set up cookie session

--- backend/src/lib/configs/app/headers.ts
+++ backend/src/lib/configs/app/headers.ts
@@ -1,6 +1,8 @@
 import { INestApplication } from '@nestjs/common';
+import helmet from 'helmet';
 
 export const setHeaders = (app: INestApplication) => {
+    app.use(helmet());
     (app as any).set('etag', false);
     app.use((req, res, next) => {
         res.removeHeader('x-powered-by');
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,11 +5,10 @@ import { setPipe } from './lib/configs/app/pipes';
 import { setCookieSession } from './lib/configs/app/cookieSession';
 import { setHeaders } from './lib/configs/app/headers';
 import path from 'path';
-import helmet from 'helmet';
 
 global.__baseDir = path.resolve(__dirname, '..', '..');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 const ENV = process.env.NODE_ENV || 'development';
 const origin = ENV === 'development' ? 'http://localhost:3000' : process.env.FRONTEND_DOMAIN;
 
@@ -19,7 +18,6 @@ export async function bootstrap() {
     setSwagger(app);
     setPipe(app);
     setCookieSession(app);
-    app.use(helmet());
     setHeaders(app);
     app.enableCors({ origin, credentials: true });
     await app.listen(PORT);

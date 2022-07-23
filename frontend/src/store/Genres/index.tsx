c54d55d88f951798df5f0345219567a820922d7b
Mikhail Drozdovich <mikhail.drozdovich@bluestone.com.au>
2022-07-23 17:10:52 +0300

    Cleanup code

--- frontend/src/store/Genres/index.tsx
+++ frontend/src/store/Genres/index.tsx
@@ -4,6 +4,7 @@ import { Genre } from 'types/track';
 import Api from 'store/core/Api';
 import { getAll } from 'api/genres';
 import { BaseRequestStore } from 'store/core/BaseRequestStore';
+import { adminPageTableLimit } from '../../settings';
 
 export class GenresStore extends BaseRequestStore<PaginatedItems<Genre>> {
     constructor(color: string) {
@@ -17,6 +18,6 @@ export class GenresStore extends BaseRequestStore<PaginatedItems<Genre>> {
     getAll(cfg?: PaginationParams, options?: RequestOptions, cb?: Function) {
         const sendRequest = new Api<PaginatedItems<Genre>>({ store: this, method: getAll }).execResult();
 
-        sendRequest({ limit: 12, ...this.meta, ...cfg }, options, cb);
+        sendRequest({ limit: adminPageTableLimit, ...this.meta, ...cfg }, options, cb);
     }
 }

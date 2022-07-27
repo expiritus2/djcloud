b7cd5562bc8401c1bc71a2c628e800d3e54592e4
Mikhail Drozdovich <mikhail.drozdovich@bluestone.com.au>
2022-07-27 23:57:43 +0300

    * Correct link by click logo * Add ability to do rating on player for current track * Add validation to Dropzone (not empty) * Link to telegram

--- frontend/src/components/Header/components/index.ts
+++ frontend/src/components/Header/components/index.ts
@@ -2,3 +2,4 @@ export { default as Logo } from './Logo';
 export { default as Navigation } from './Navigation';
 export { default as LoginAvatar } from './LoginAvatar';
 export { default as Search } from './Search';
+export { default as TelegramLink } from './TelegramLink';

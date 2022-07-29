ca648d6901d0ebe5cdf220a68cbdbd63ba6e893b
Mikhail Drozdovich <mikhail.drozdovich@bluestone.com.au>
2022-07-30 01:59:07 +0300

    Revert database to heroku

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -47,8 +47,7 @@ switch (process.env.ENVIRONMENT) {
             migrationsRun: true,
             migrations: ['migrations/*.js', `migrations/${EnvEnums.PRODUCTION}/*.js`],
             ssl: {
-                rejectUnauthorized: true,
-                ca: process.env.CA_CERT,
+                rejectUnauthorized: false,
             },
         });
         break;

0b53fc71455c465039b46c95a00adef7fcd32240
Mikhail Drozdovich <mikhail.drozdovich@bluestone.com.au>
2022-07-30 00:55:19 +0300

    Fix ormconfig prod dev

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -23,7 +23,7 @@ switch (process.env.ENVIRONMENT) {
             database: 'local',
             entities: ['**/*.entity.js'],
             migrationsRun: true,
-            migrations: ['migrations/*.js', `migrations/${EnvEnums.DEVELOPMENT}/*.js`],
+            migrations: ['migrations/*.js', 'migrations/dev/*.js'],
         } as TypeOrmModuleOptions);
         break;
     case EnvEnums.TEST:
@@ -36,7 +36,7 @@ switch (process.env.ENVIRONMENT) {
             database: 'test',
             entities: ['**/*.entity{.ts,.js}'],
             migrationsRun: true,
-            migrations: ['migrations/*.js', `migrations/${EnvEnums.TEST}/*.js`],
+            migrations: ['migrations/*.js', 'migrations/test/*.js'],
         });
         break;
     case EnvEnums.PRODUCTION:
@@ -45,7 +45,7 @@ switch (process.env.ENVIRONMENT) {
             url: process.env.DATABASE_URL,
             entities: ['**/*.entity.js'],
             migrationsRun: true,
-            migrations: ['migrations/*.js', `migrations/${EnvEnums.PRODUCTION}/*.js`],
+            migrations: ['migrations/*.js', 'migrations/prod/*.js'],
             ssl: {
                 rejectUnauthorized: false,
             },

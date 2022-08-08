eb117b6c6aa75fe1bd660ab50d9d5ad36dce897a
Mikhail Drozdovich <mikhail.drozdovich@bluestone.com.au>
2022-08-09 00:37:28 +0300

    Confirm message send track to telegram

--- frontend/src/components/TableActions/SendToTelegram/index.tsx
+++ frontend/src/components/TableActions/SendToTelegram/index.tsx
@@ -1,11 +1,14 @@
 import React, { FC, useState } from 'react';
 import classNames from 'classnames';
 
-import styles from './styles.module.scss';
 import { FaTelegram } from 'react-icons/fa';
 import { Track } from 'types/track';
 import Action from '../Action';
-import { useStore } from '../../../store';
+import { useStore } from 'store';
+import { ContentModal } from '../..';
+import { ButtonType } from '../../ContentModal';
+
+import styles from './styles.module.scss';
 
 type ComponentProps = {
     className?: string;
@@ -16,23 +19,61 @@ type ComponentProps = {
 const SendToTelegram: FC<ComponentProps> = (props) => {
     const { className, iconClassName, track } = props;
     const { tracks } = useStore();
+    const [openModal, setOpenModal] = useState(false);
     const [isSendToTelegramPending, setIsSetToTelegramPending] = useState(false);
 
     const onClick = () => {
+        setOpenModal(true);
+    };
+
+    const onClickCancel = () => {
+        setOpenModal(false);
+    };
+
+    const onClickSubmit = () => {
         setIsSetToTelegramPending(true);
         tracks.sendToTelegram({ trackId: track.id }, {}, () => {
             setIsSetToTelegramPending(false);
+            setOpenModal(false);
         });
     };
 
+    const buttons: ButtonType[] = [
+        { id: 'cancel', onClick: onClickCancel, label: 'Cancel', variant: 'secondary' },
+        {
+            id: 'submit',
+            formId: 'trackSubmit',
+            type: 'submit',
+            onClick: onClickSubmit,
+            label: 'Submit',
+            variant: 'primary',
+            pending: isSendToTelegramPending,
+        },
+    ];
+
     return (
-        <Action
-            className={classNames(styles.sendToTelegram, className)}
-            isPending={isSendToTelegramPending}
-            loaderClassName={styles.telegramLoader}
-        >
-            <FaTelegram onClick={onClick} id="telegramIcon" className={iconClassName} />
-        </Action>
+        <div>
+            <Action
+                className={classNames(styles.sendToTelegram, className)}
+                isPending={isSendToTelegramPending}
+                loaderClassName={styles.telegramLoader}
+            >
+                <FaTelegram onClick={onClick} id="telegramIcon" className={iconClassName} />
+            </Action>
+            <ContentModal
+                open={openModal}
+                title="Confirm send to telegram"
+                buttons={buttons}
+                className={classNames(styles.modal, className)}
+            >
+                <div className={styles.content}>
+                    <span className={styles.message}>
+                        Are you sure you want to send track: <span className={styles.accent}>{track.title}</span> to
+                        telegram?
+                    </span>
+                </div>
+            </ContentModal>
+        </div>
     );
 };
 

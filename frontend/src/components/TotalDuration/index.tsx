9d25fd2e6899386652280a3847da37899b870cda
Mikhail <michaildrazdovich@gmail.com>
2022-08-14 14:24:14 +0300

    Show total count tracks with total duration (#74)
    Co-authored-by: Mikhail Drozdovich <mikhail.drozdovich@bluestone.com.au>

--- frontend/src/components/TotalDuration/index.tsx
+++ frontend/src/components/TotalDuration/index.tsx
@@ -18,7 +18,7 @@ const TotalDuration: FC<ComponentProps> = (props) => {
     return (
         <div className={classNames(styles.totalDuration, className)}>
             <span className={styles.totalText}>Total Duration: </span>
-            <span>{getDuration(tracks.data?.totalDuration || 0)}</span>
+            <span>{`${getDuration(tracks.data?.totalDuration || 0)} (${tracks.data?.count})`}</span>
         </div>
     );
 };

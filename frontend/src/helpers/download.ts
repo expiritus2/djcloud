088d3dacb8a15de02281b91be78ec5212ecfe2cc
Mikhail Drozdovich <mikhail.drozdovich@bluestone.com.au>
2022-09-09 17:50:17 +0300

    Improve download

--- frontend/src/helpers/download.ts
+++ frontend/src/helpers/download.ts
@@ -19,7 +19,7 @@ export const getUrlExtension = (url: string) => {
 
 export const downloadByRequest = (fileUrl: string, name: string, cb?: Function) => {
     axios({ url: fileUrl, method: 'GET', responseType: 'blob' }).then((response) => {
-        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const url = window.URL.createObjectURL(response.data);
         const link = document.createElement('a');
         link.href = url;
         link.setAttribute('download', `${name}.${getUrlExtension(fileUrl)}`); //or any other extension
